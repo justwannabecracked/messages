5d8d017ee2680d9c0d274de8cda5c1da8bcb09ae
Femi Obadimu <ficazzo@Femis-MacBook-Air.local>
2023-05-02 00:12:13 +0100

    Ready to push

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
-  await app.listen(3000);
+  await app.listen(8000, () => {
+     console.log("Server Started Successfully");
+     
+  });
 }
 bootstrap();
