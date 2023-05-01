293452c016f648f08d0659e790d89b2742834755
Femi Obadimu <ficazzo@Femis-MacBook-Air.local>
2023-05-02 00:24:44 +0100

    Ready to push-Tuesday 002 - 00:24 am

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Get,Post } from '@nestjs/common';
+import { Controller,Get,Post,Body, Param} from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -7,11 +7,13 @@ export class MessagesController {
 
     }
     @Post()
-    createMessage(){
+    createMessage(@Body() body: any){
+    console.log(body);
 
     }
     @Get("/:id")
-    getMessage(){
+    getMessage(@Param("id") id: string){
+    console.log(id);
 
     }
 }
